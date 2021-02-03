3887c63cfc8e822ad8ccccb09dfc9f73c046ecf3
warmwhalefy <angelhfy1508@gmail.com>
2021-02-03 15:20:54 +0800

    Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 
 public class Duke {
-    static String[] StringArray= new String[100];
     static int noOfTasks = 0;
     static String sectionDivider = "____________________________________________________________";
     public static void main(String[] args) {
+        Task TaskArray[] = new Task[100];
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -13,7 +13,7 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println(sectionDivider);
         System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
+        System.out.println("Please type tasks to do OR (list) to list all the tasks OR (bye) to exit.");
         Scanner sc= new Scanner(System.in);
         while (true) {
             String str = sc.nextLine();
@@ -25,14 +25,26 @@ public class Duke {
             }
             if (str.equals("list")){
                 System.out.println(sectionDivider);
+                System.out.println("Here are the tasks in your to-do list:");
+                System.out.println(TaskArray[0].getStatusIcon());
                 for(int i= 1; i!=noOfTasks+1; i++){
-                    System.out.println(i + "." + StringArray[i-1]);
+                    System.out.println(i + ". [" + TaskArray[i-1].getStatusIcon() + " ]" + TaskArray[i-1].getDescription());
                 }
+                System.out.println(sectionDivider);
+            }
+            if (str.contains("done")){
+                System.out.println(sectionDivider);
+                String[] splitInput = str.split(" ");
+                int taskNumber = Integer.parseInt(splitInput[1]);
+                TaskArray[taskNumber-1].markAsDone();
+                System.out.println("Nice! I've marked this task as done: ");
+                System.out.println(taskNumber + ". [" + TaskArray[taskNumber-1].getStatusIcon() + " ]" + TaskArray[taskNumber-1].getDescription());
                 System.out.println(sectionDivider);
             }
             else {
                 System.out.println(sectionDivider);
-                StringArray[noOfTasks]= str;
+                Task t = new Task(str);
+                TaskArray[noOfTasks] = t;
                 noOfTasks = noOfTasks + 1;
                 System.out.println("added: " + str);
                 System.out.println(sectionDivider);

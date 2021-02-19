33e09585688264007beeead2b1f003fdc06b6bda
warmwhalefy <angelhfy1508@gmail.com>
2021-02-19 12:57:12 +0800

    level7

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,172 +1,191 @@
+package duke;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-    static int noOfTasks = 0;
-    static String sectionDivider = "____________________________________________________________";
+    public static final int INDEX_TODO = 5;
+    public static final int INDEX_EVENT = 6;
+    public static final int INDEX_DEADLINE = 9;
+    public static final String SECTION_DIVIDER = "____________________";
     public static boolean invalidInput = false;
+    private static ArrayList<Task> TaskArray = new ArrayList<>();
 
     public static void main(String[] args) {
-        Task[] TaskArray = new Task[100];
-        Task newItem = null;
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
+        String logo = " __        _        \n"
+                + "|  _ \\ _   | | __ \n"
                 + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+                + "| || | || |   <  __/\n"
+                + "|_/ \\,||\\\\___|\n";
         System.out.println("Hello from\n" + logo);
-        System.out.println(sectionDivider);
-        System.out.println("Hello! I'm Duke");
-        System.out.println("Please type tasks to do OR (list) to list all the tasks OR (bye) to exit.");
-        Scanner sc= new Scanner(System.in);
+        System.out.println("Hello! I'm Duke.");
+        System.out.println("What can I do for you?");
+        System.out.println(SECTION_DIVIDER);
+
+        Scanner scan = new Scanner(System.in);
         String userInput;
-
-        userInput = getUserInput(sc);
-
-        // Clean text input, handle errors
+        userInput = getUserInput(scan);
         while (invalidInput) {
-            userInput = getUserInput(sc);
+            userInput = getUserInput(scan);
         }
-
-        while (true) {
-            if (userInput.equals("bye")) {
-                System.out.println(sectionDivider);
-                System.out.println("Bye. Hope to see you again soon!");
-                System.out.println(sectionDivider);
-                break;
-            }
-            if (userInput.equals("list")){
-                System.out.println(sectionDivider);
-                System.out.println("Here are the tasks in your to-do list:");
-                for(int i= 1; i!=noOfTasks+1; i++){
-                    System.out.println(i + "." + TaskArray[i-1].toString());
-                }
-                System.out.println(sectionDivider);
-            }
-            if (userInput.contains("done")){
-                System.out.println(sectionDivider);
-                String[] splitInput = userInput.split(" ");
-                int taskNumber = Integer.parseInt(splitInput[1]);
-                TaskArray[taskNumber-1].markAsDone();
-                System.out.println("Nice! I've marked this task as done: ");
-                System.out.println(taskNumber + ". [" + TaskArray[taskNumber-1].getStatusIcon() + " ]" + TaskArray[taskNumber-1].getDescription());
-                System.out.println(sectionDivider);
+        while (sayBye(userInput) == 0) {
+            if (userInput.contains("list")) {
+                viewTasks(TaskArray);
+            } else if (userInput.contains("done")) {
+                markTaskAsDone(userInput);
+            } else if (userInput.contains("delete")) {
+                deleteTask(userInput);
             } else {
-                System.out.println(sectionDivider);
-                String[] splitInput = userInput.split(" ");
-                if (splitInput[0].equals("deadline")) {
-                    int wordsCount = splitInput.length;
-                    String descriptionString = "";
-                    String byString = "";
-                    int byIndex = 0;
-                    for (int i = 1; i != wordsCount; i++) {
-                        if (splitInput[i].equals("/by")) {
-                            byIndex = i + 1;
-                            break;
-                        } else {
-                            descriptionString = descriptionString + splitInput[i] + " ";
-                        }
-                    }
-                    while (byIndex != wordsCount) {
-                        byString = byString + splitInput[byIndex] + " ";
-                        byIndex++;
-                    }
-                    newItem = new Deadline(descriptionString, byString);
-                    TaskArray[noOfTasks] = newItem;
-                    noOfTasks = noOfTasks + 1;
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println(TaskArray[noOfTasks - 1].toString());
-                    System.out.println("Now you have " + noOfTasks + " tasks in the list.");
-                    System.out.println(sectionDivider);
-                }
-                if (splitInput[0].equals("event")) {
-                    int wordsCount = splitInput.length;
-                    String descriptionString = "";
-                    String atString = "";
-                    int atIndex = 0;
-                    for (int i = 1; i != wordsCount; i++) {
-                        if (splitInput[i].equals("/at")) {
-                            atIndex = i + 1;
-                            break;
-                        } else {
-                            descriptionString = descriptionString + splitInput[i] + " ";
-                        }
-                    }
-                    while (atIndex != wordsCount) {
-                        atString = atString + splitInput[atIndex] + " ";
-                        atIndex++;
-                    }
-                    newItem = new Event(descriptionString, atString);
-                    TaskArray[noOfTasks] = newItem;
-                    noOfTasks = noOfTasks + 1;
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println(TaskArray[noOfTasks - 1].toString());
-                    System.out.println("Now you have " + noOfTasks + " tasks in the list.");
-                    System.out.println(sectionDivider);
-                }
-                if (splitInput[0].equals("todo")) {
-                    int wordsCount = splitInput.length;
-                    String descriptionString = "";
-                    for (int i = 1; i != wordsCount; i++) {
-                        descriptionString = descriptionString + splitInput[i] + " ";
-                    }
-                    newItem = new Todo(descriptionString);
-                    TaskArray[noOfTasks] = newItem;
-                    noOfTasks = noOfTasks + 1;
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println(TaskArray[noOfTasks - 1].toString());
-                    System.out.println("Now you have " + noOfTasks + " tasks in the list.");
-                    System.out.println(sectionDivider);
-                }
+                addTaskToArray(userInput);
+            }
+            userInput = getUserInput(scan);
+            while (invalidInput) {
+                userInput = getUserInput(scan);
             }
         }
     }
-    public static String getUserInput(Scanner sc) {
+
+    public static String getUserInput(Scanner scan) {
         String userInput;
-        userInput = sc.nextLine();
+        userInput = scan.nextLine();
         try {
             handleErrorUserInputs(userInput);
         } catch (TaskException e) {
             invalidInput = true;
-            System.out.println("☹ OOPS!!! The description of a todo cannot be empty.");
-            System.out.println(sectionDivider);
+            System.out.println("☹️ OOPS!!! The description of a todo cannot be empty.");
+            System.out.println(SECTION_DIVIDER);
         } catch (EventException e) {
             invalidInput = true;
-            System.out.println("☹ OOPS!!! The description of an event cannot be empty or is incomplete (/at).");
-            System.out.println(sectionDivider);
+            System.out.println("☹️ OOPS!!! The description of an event cannot be empty or is incomplete (/at).");
+            System.out.println(SECTION_DIVIDER);
         } catch (DeadlineException e) {
             invalidInput = true;
-            System.out.println("☹ OOPS!!! The description of a deadline cannot be empty or is incomplete (/by).");
-            System.out.println(sectionDivider);
+            System.out.println("☹️ OOPS!!! The description of a deadline cannot be empty or is incomplete (/by).");
+            System.out.println(SECTION_DIVIDER);
         }
         return userInput;
     }
+
+
+    public static void addTaskToArray(String userInput) {
+        Task taskAdded = null;
+        if (userInput.contains("deadline")) {
+            taskAdded = addDeadlineToArray(userInput);
+        } else if (userInput.contains("event")) {
+            taskAdded = addEventToArray(userInput);
+        } else if (userInput.contains("todo")){
+            taskAdded = addTodoToArray(userInput);
+        }
+        System.out.println("Got it. I've added this task:" + System.lineSeparator() + taskAdded);
+        System.out.println("Now you have " + TaskArray.size() + " tasks in the list.");
+        System.out.println(SECTION_DIVIDER);
+    }
+
+    public static Todo addTodoToArray(String userInput) {
+        int descriptionIndex = userInput.indexOf("todo") + INDEX_TODO;
+        Todo todoAdded = new Todo(userInput.substring(descriptionIndex));
+        TaskArray.add(todoAdded);
+        return todoAdded;
+    }
+
+    public static Event addEventToArray(String userInput) {
+        int descriptionIndex = userInput.indexOf("event") + INDEX_EVENT;
+        int byIndex = userInput.indexOf("/at");
+        Event eventAdded = new Event(userInput.substring(descriptionIndex, byIndex-1), userInput.substring(byIndex + 4));
+        TaskArray.add(eventAdded);
+        return eventAdded;
+    }
+
+    public static Deadline addDeadlineToArray(String userInput) {
+        int descriptionIndex = userInput.indexOf("deadline") + INDEX_DEADLINE;
+        int byIndex = userInput.indexOf("/by");
+        Deadline deadlineAdded = new Deadline(userInput.substring(descriptionIndex, byIndex-1), userInput.substring(byIndex + 4));
+        TaskArray.add(deadlineAdded);
+        return deadlineAdded;
+    }
+
+    public static void markTaskAsDone(String userInput) {
+        userInput = userInput.replaceAll("[^0-9]", "");
+        try {
+            int taskNo = Integer.parseInt(userInput);
+            if (taskNo > TaskArray.size()) {
+                System.out.println("You only have " + TaskArray.size() + " task(s)!");
+            } else {
+                TaskArray.get(taskNo - 1).markAsDone();
+                System.out.println("Nice! I have marked this task as done:" + TaskArray.get(taskNo - 1));
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid Input! Input format should have an integer e.g. done 2");
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Invalid Input! Integer cannot be 0!");
+        }
+        System.out.println(SECTION_DIVIDER);
+    }
+
+    private static void deleteTask(String userInput) {
+        userInput = userInput.replaceAll("[^0-9]", "");
+        try {
+            int taskNo = Integer.parseInt(userInput);
+            if (taskNo > TaskArray.size()) {
+                System.out.println("You only have " + TaskArray.size() + " task(s)!");
+            } else {
+                System.out.println("Noted. I've removed this task: " + System.lineSeparator() + TaskArray.get(taskNo - 1));
+                TaskArray.remove(taskNo - 1);
+                System.out.println("Now you have " + TaskArray.size() + " tasks in the list.");
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid Input! Input format should have an integer e.g. done 2");
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Invalid Input! Integer cannot be 0!");
+        } finally {
+            System.out.println(SECTION_DIVIDER);
+        }
+    }
+
+    public static void viewTasks(ArrayList<Task> arrayOfTasks) {
+        if (arrayOfTasks.size() == 0) {
+            System.out.println("There are no tasks in your list.");
+        } else {
+            System.out.println("Here are the tasks in your list:");
+            for (int i = 1; i <= arrayOfTasks.size(); i++) {
+                System.out.println(i + ". " + arrayOfTasks.get(i - 1));
+            }
+        }
+        System.out.println(SECTION_DIVIDER);
+    }
+
+    public static int sayBye(String bye) {
+        if (bye.toLowerCase().contains("bye")) {
+            System.out.println("Bye. Hope to see you again soon!");
+            System.out.println(SECTION_DIVIDER);
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
     public static void handleErrorUserInputs(String userInput) throws TaskException, EventException, DeadlineException {
         userInput = userInput.toLowerCase().trim();
 
-        // check if todo description is empty
         if (userInput.contains("todo") && !(userInput.contains("event")) && !(userInput.contains("deadline"))) {
             validateTodo(userInput);
-            // check if event description is empty
         } else if (userInput.contains("event") && !(userInput.contains("todo")) && !(userInput.contains("deadline"))) {
             validateEvent(userInput);
-            // check if deadline description is empty
         } else if (userInput.contains("deadline") && !(userInput.contains("todo")) && !(userInput.contains("event"))) {
             validateDeadline(userInput);
-            // check if keyword were used in userInput
         } else if (userInput.contains("list")
                 || userInput.contains("bye")
                 || userInput.contains("done")
                 || userInput.contains("delete")) {
             invalidInput = false;
-            // incorrect input
         } else {
             invalidInput = true;
-            System.out.println("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
-            System.out.println(sectionDivider);
+            System.out.println("☹️ OOPS!!! I'm sorry, but I don't know what that means :-(");
+            System.out.println(SECTION_DIVIDER);
         }
     }
+
     public static void validateDeadline(String userInput) throws DeadlineException {
-        if (userInput.substring(9 - 1).trim().isEmpty()
+        if (userInput.substring(INDEX_DEADLINE - 1).trim().isEmpty()
                 || !(userInput.contains("/by"))
                 || userInput.substring(userInput.indexOf("/by") + 3).trim().isEmpty() ) {
             throw new DeadlineException();
@@ -176,7 +195,7 @@ public class Duke {
     }
 
     public static void validateEvent(String userInput) throws EventException {
-        if (userInput.substring(6 - 1).trim().isEmpty()
+        if (userInput.substring(INDEX_EVENT - 1).trim().isEmpty()
                 || !(userInput.contains("/at"))
                 || userInput.substring(userInput.indexOf("/at") + 3).trim().isEmpty()) {
             throw new EventException();
@@ -186,7 +205,7 @@ public class Duke {
     }
 
     public static void validateTodo(String userInput) throws TaskException {
-        if (userInput.substring(5 - 1).trim().isEmpty()) {
+        if (userInput.substring(INDEX_TODO - 1).trim().isEmpty()) {
             throw new TaskException();
         } else {
             invalidInput = false;
